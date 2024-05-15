8e644f67f184922667fc11cacceaf3f1b1ad51d0
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-05-15 13:19:33 +0500

    data ingestion added

---
+++ main.py
@@ -0,0 +1,15 @@
+from textSummarizer.logging import logger
+
+from textSummarizer.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline 
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
